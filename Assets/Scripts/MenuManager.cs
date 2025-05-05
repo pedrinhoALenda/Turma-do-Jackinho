bb2fb88598b813de7be611610dc48abeab375d3c
youngpetter <britop03@gmail.com>
2025-05-05 15:37:23 -0300

    .4

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimpleMenu : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
